446494b6458c63ea5348ed16e9135c8e59d9e385
Boli2Eliel <popaulnzongo@gmail.com>
2023-08-22 12:47:11 +0100

    registration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+def home(request):
+    # Check to see if logging in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
 
-# Create your views here.
+        #Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Bienvenue à PrestCRM!")
+            return redirect('home')
+        else:
+            messages.error(request, "Il y a une erreur, veuillez réessayer svp.")
+            return redirect('home')
+    else:
+        return render(request, 'website/home.html')
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Vous êtes déconnecté...")
+    return redirect('home')
+
+def register_user(request):
+    return render(request, 'website/register.html')
